d7503d318af3392dbedd7681fb24098441505659
Logan Moore <lmooore@gmail.com>
2020-10-29 20:26:56 -0700

    make it init for real

--- src/state/index.mjs
+++ src/state/index.mjs
@@ -1,5 +1,4 @@
-// export { default as init } from './init.mjs';
-export const init = () => ({});
+export { default as init } from './init.mjs';
 export { default as lean } from './lean.mjs'
 export { default as move } from './move.mjs'
 export { default as normalizeInput } from './normalize-input.mjs'
